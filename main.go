b0cd2187814dd0f0cbaabfd0e6d936b9f111a7dc
hisyntax <hisyntax0@gmail.com>
2021-09-09 12:19:37 +0100

    Procfile created

--- main.go
+++ main.go
@@ -9,12 +9,21 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var tpl = template.Must(template.ParseFiles("templates/index.html"))
+var welcome = template.Must(template.ParseFiles("templates/welcome.html"))
+var index = template.Must(template.ParseFiles("templates/index.html"))
+var gallery = template.Must(template.ParseFiles("templates/gallery.html"))
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	tpl.Execute(w, nil)
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	welcome.Execute(w, nil)
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	index.Execute(w, nil)
+}
+
+func galleryHandler(w http.ResponseWriter, r *http.Request) {
+	gallery.Execute(w, nil)
+}
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -28,7 +37,9 @@ func main() {
 	fs := http.FileServer(http.Dir("public"))
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/", welcomeHandler)
+	mux.HandleFunc("/welcome", indexHandler)
+	mux.HandleFunc("/gallery", galleryHandler)
 	mux.Handle("/public/", http.StripPrefix("/public", fs))
 	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
